b0de9865401be86cb5614054680f802262dbb731
Ubuntu <ubuntu@ip-172-31-41-70.sa-east-1.compute.internal>
2015-11-01 02:34:35 +0000

    scale

--- politicos/wsgi.py
+++ politicos/wsgi.py
@@ -7,7 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+
+import os,sys
+
+path = '/var/www/politicos'
+if path not in sys.path:
+    sys.path.insert(0, path) 
+
 
 from django.core.wsgi import get_wsgi_application
 
