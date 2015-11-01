a6d0444bc965e2fd9654dcd3678c238f213f8948
Kziete <kziete@gmail.com>
2015-10-31 23:21:24 -0300

    admin

--- politicos/urls.py
+++ politicos/urls.py
@@ -19,9 +19,10 @@ from django.contrib import admin
 from sitio import views
 
 urlpatterns = [
-	url(r'^$', views.Home.as_view(), name='home'),
-	url(r'^(?P<slug>[-\w]+)$',views.DetallePolitico.as_view(),name="detalle-politico"),
-    url(r'^(?P<politico>[-\w]+)/(?P<slug>[-\w]+)$',views.DetalleEvento.as_view(),name="detalle-evento"),
-	
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', views.Home.as_view(), name='home'),
+    url(r'^(?P<slug>[-\w]+)$',views.DetallePolitico.as_view(),name="detalle-politico"),
+    url(r'^(?P<politico>[-\w]+)/(?P<slug>[-\w]+)$',views.DetalleEvento.as_view(),name="detalle-evento"),
+    
 ]
