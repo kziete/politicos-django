704a788f19fbab405a576a5ac5f4f94812979d14
Kziete <kziete@gmail.com>
2015-11-01 23:03:30 -0300

    denuncia casi

--- sitio/views.py
+++ sitio/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView
 from django.db.models import Count
 from django import forms
+from django.core.urlresolvers import reverse
+
+import json
 
 from datos.models import *
 
@@ -20,5 +24,14 @@ class DetalleEvento(DetailView):
 	model = Evento
 
 class DenunciaEvento(CreateView):
-	fields = ("nombre","fuente")
+	fields = ("nombre","fuente","autor","fecha")
 	model = Evento
+
+	def form_valid(self,form):
+		data = form.save(commit=False)
+		data.politico = Politico.objects.get(slug=self.kwargs["politico"])
+
+		data.save()
+
+		return HttpResponseRedirect(reverse('detalle-politico', kwargs={"slug" :data.politico.slug}))
+
